fa477cc3a349f0ca0ed372279d0dce633dbe5a52
Phelps <phelpsmemo@gmail.com>
2021-12-12 15:50:21 -0300

    resource config and parsers for resources (Closes #8)

---
+++ storm_commons/resources/config.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021 Storm Project.
+#
+# storm-commons is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+from flask_resources import (
+    JSONDeserializer,
+    JSONSerializer,
+    RequestBodyParser,
+    ResourceConfig,
+    ResponseHandler,
+)
+
+
+class ResourceConfigBase(ResourceConfig):
+    """Base configuration class for job resources."""
+
+    # Request parsing
+    request_read_args = {}
+    request_view_args = {}  # defined in the concrete implementations.
+    request_body_parsers = {"application/json": RequestBodyParser(JSONDeserializer())}
+    default_content_type = "application/json"
+
+    # Response handling
+    response_handlers = {"application/json": ResponseHandler(JSONSerializer())}
+    default_accept_mimetype = "application/json"
